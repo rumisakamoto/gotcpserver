4b27b83c2f9ad2bd46f06c55c9d30a30b985321a
sakamoto rumi <lily.my.sun@gmail.com>
2016-08-15 15:23:27 +0900

    posgtres connection added

--- server.go
+++ server.go
@@ -3,19 +3,29 @@ import (
     "fmt"
     "net"
     "os"
+    _ "github.com/lib/pq"
+    "database/sql"
+    "strconv"
 )
 const (
     CONN_HOST = "localhost"
     CONN_PORT = "3333"
     CONN_TYPE = "tcp"
+    DB_TYPE = "postgres"
+    DB_NAME = "testdb" // TODO change
+    DB_USER = "postgres"
+    DB_PSWD = "postgres"
+    DB_HOST = "localhost"
+    DB_QUERY = "SELECT id FROM testtable WHERE id=$1" // TODO change
 )
+type Record struct {
+    id int
+}
 func main() {
+
     // listen incoming connections.
     l, err := net.Listen(CONN_TYPE, CONN_HOST + ":" + CONN_PORT)
-    if err != nil {
-        fmt.Println("Listening error: ", err.Error())
-        os.Exit(1)
-    }
+    checkError(err, "Listening error: %s")
     // close the listener when the application closes.
     defer l.Close()
 
@@ -23,10 +33,7 @@ func main() {
     for {
         // listen an incoming connection
         conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Accepting error: ", err.Error())
-            os.Exit(1)
-        }
+        checkError(err, "Accepting error: %s")
         // handle connections in a new goroutine
         go handleRequest(conn)
     }
@@ -37,14 +44,41 @@ func handleRequest(conn net.Conn) {
 
     // read the incoming connection into the buffer
     reqLen, err := conn.Read(buf)
-    if err != nil {
-        fmt.Println("Incoming connection reading error :", err.Error())
-    }
+    checkError(err, "Incoming connection reading error: %s")
+
     receivedData := string(buf[:reqLen])
     fmt.Println(receivedData)
 
+    data := getDeviceData("1")
+
     // send a response
-    conn.Write([]byte("Message received."))
+    conn.Write([]byte(strconv.Itoa(data.id)))
 
     conn.Close()
+}
+func getDeviceData(id string) Record {
+    // open databse connection.
+    db, err := sql.Open(DB_TYPE, "user="+DB_USER+" dbname="+DB_NAME+" password="+DB_PSWD+" host="+DB_HOST+" sslmode=disable")
+    checkError(err, "Opening database connection is failed: %s")
+
+    err = db.Ping()
+    checkError(err, "Establishing database connection is failed: %s")
+
+    query, err := db.Prepare(DB_QUERY)
+    checkError(err, "Prepareing query statement is failed: %s")
+
+    var r Record
+    err = query.QueryRow(id).Scan(&r.id)
+    checkError(err, "Getting data is failed: %s")
+
+    db.Close()
+
+    return r
+}
+func checkError(err error, msg string) {
+    if err != nil {
+      //fmt.Fprintf(os.Stderr, msg, err.Error())
+      fmt.Println(msg, err.Error())
+      os.Exit(1)
+    }
 }
